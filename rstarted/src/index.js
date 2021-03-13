5fcd3f9e4e872322de20cf37cb404027943466af
Vikrant Sharma <iamvikrant1@gmail.com>
2021-03-14 04:06:46 +0530

    First update

--- rstarted/src/index.js
+++ rstarted/src/index.js
@@ -1,17 +1,24 @@
+//Import the React and React-DOM libraries 
+//Import everything which is needed for this exact js file
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//Create a react component
+//Function component and Class based component
+
+const App = () =>{
+    const buttonText = 'Click Me!';
+    return (
+        <div> 
+            <label className = "label" htmlFor="name"> Enter the name !</label>
+            <input id="name" style={{padding: '1px'}} type="text"/>
+            <button style={{backgroundColor: 'red', border: '1px solid black'}}>{buttonText}</button>
+        </div>
+    );
+};
+
+//Take the react component and show it on screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App/>,
+    document.querySelector('#root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
